50ff10b0e5fbd79d0656dc1b3bb94d46d6250f05
Administrator <Administrator@304-t>
2022-10-19 13:08:20 +0900

    1019-1

--- src/main/resources/static/js/visual.js
+++ src/main/resources/static/js/visual.js
@@ -3,10 +3,25 @@
  */
 
 //var size=415;//이미지사이즈(400)+15
+
 var marginLeft=415;
+var basePos=marginLeft*6;
 var speed=1000;
 var timmer=3000;
 var myTimeout;
+//ready()
+$(function(){
+	$(".visuals").css("margin-left", -basePos);
+	start();
+	
+	$(".cover .center").hover(function(){
+		stop();
+	},function(){
+		start();
+	});
+	
+});
+
 //브라우저의 화면이 보여지냐 숨겨지냐에 따라 실행되는 이벤트
 document.addEventListener("visibilitychange", function() {
 	console.log( document.visibilityState );
@@ -16,10 +31,7 @@ document.addEventListener("visibilitychange", function() {
 		start();
 	}
 });
-//ready()
-$(function(){
-	start();
-});
+
 function start(){
 	myTimeout = setTimeout(run, timmer);
 }
@@ -36,14 +48,14 @@ function stop(){
 function move(){
 	var imgWrap=$(".visuals");
 	//marginLeft += size;
-	imgWrap.animate({marginLeft: -marginLeft},speed, function(){
+	imgWrap.animate({marginLeft: -(marginLeft+basePos)},speed, function(){
 		var lis=$("ol.visuals li");
 		//var lastLi=lis.last();
 		//var firstLi=lis.first();
 		//lastLi.after(firstLi);
 		//첫번째 li태그가 마지막 li태그뒤로 이동
 		lis.last().after(lis.first());
-		imgWrap.css("margin-left", 0);
+		imgWrap.css("margin-left", -basePos);
 	});
 }
 
