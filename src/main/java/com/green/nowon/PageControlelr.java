428f56c2c452bbcdf7d2342691c09355b1d5cff1
Administrator <Administrator@304-t>
2022-10-20 17:32:51 +0900

    1020-7

--- src/main/java/com/green/nowon/PageControlelr.java
+++ src/main/java/com/green/nowon/PageControlelr.java
@@ -2,6 +2,8 @@ package com.green.nowon;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageControlelr {
@@ -20,5 +22,21 @@ public class PageControlelr {
 	}
 	@GetMapping("/test2")// classpath:/templates/test2.html
 	public void test2() {
+	}
+	
+	@GetMapping("/goods/sale/{kind}")
+	public ModelAndView goodsSale(@PathVariable int kind, ModelAndView modelAndView) {
+		System.out.println("tab메뉴를 누르면 실행됩니다.");
+		modelAndView.setViewName("goods/sale/list");
+		if(kind==1) {
+			modelAndView.addObject("list", "타임특가 상품");
+		}else if(kind==2) {
+			modelAndView.addObject("list", "한정특가 상품");
+		}else if(kind==3) {
+			modelAndView.addObject("list", "브랜드특가 상품");
+		}
+		
+		return modelAndView;
+		
 	}
 }
