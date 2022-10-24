0e0ffd47c01b343d85c5e11f6e83094c90412875
Administrator <Administrator@304-t>
2022-10-24 18:05:07 +0900

    1024-8교시

---
+++ src/main/resources/static/js/terms.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+$(function(){
+	$("#terms-all").click(termsAllClicked);
+	$(".terms :checkbox").not("#terms-all").click(termsEachClicked);
+}); 
+
+
+function termsAllClicked(){
+	// $(this).attr("checked") //HTML태그기준으로 읽어들인다.
+	//console.log("attr:"+$(this).prop("checked"));
+	if($(this).prop("checked")){
+		$(".terms :checkbox").prop("checked", true);
+	}else{
+		$(".terms :checkbox").prop("checked", false);
+	}
+}
+
+function termsEachClicked(){
+	
+	if($(this).prop("checked")){
+		//true일떄는 다른 checked도 모두true이면 
+		console.log("체크박스 ture일때 나머지 체크");
+		var tot=$(".terms :checkbox:not('#terms-all'").length;
+		var checkdTot=$(".terms :checkbox:not('#terms-all'):checked").length
+			
+		//나머지도 모두 true가 확인
+		if(tot==checkdTot){
+			$("#terms-all").prop("checked", true);
+		}else{
+			$("#terms-all").prop("checked", false);
+		}
+		
+	}else{
+		console.log("체크박스 false일때 전체박스 무조건 false");
+		$("#terms-all").prop("checked", false);
+	}
+}
