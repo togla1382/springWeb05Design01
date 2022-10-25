3e5b38f470bdfac8013fa0919dbb3e1877c61258
Administrator <Administrator@304-t>
2022-10-25 10:11:14 +0900

    1025-1교시

--- src/main/resources/static/js/terms.js
+++ src/main/resources/static/js/terms.js
@@ -19,9 +19,10 @@ function termsAllClicked(){
 
 function termsEachClicked(){
 	
+	
 	if($(this).prop("checked")){
 		//true일떄는 다른 checked도 모두true이면 
-		console.log("체크박스 ture일때 나머지 체크");
+		//console.log("체크박스 ture일때 나머지 체크");
 		//체크박스 전체개수
 		var tot=$(".terms :checkbox").not("#terms-all").length;
 		//체크된 체크박스개수
@@ -29,8 +30,10 @@ function termsEachClicked(){
 			
 		//나머지도 모두 true가 확인
 		if(tot==checkdTot){
+			//모두 true이면 전제동의 체크박스도 checked 설정
 			$("#terms-all").prop("checked", true);
 		}else{
+			//모두 true아닌경우 전제동의 체크박스도 checked 해제
 			$("#terms-all").prop("checked", false);
 		}
 		
