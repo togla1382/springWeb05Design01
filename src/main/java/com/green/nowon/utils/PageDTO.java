8f561c1c17efccc1ea2acbc3ec4ddb47a00e9c61
Administrator <Administrator@304-t>
2022-10-27 17:57:03 +0900

    1027-마지막 수정

--- src/main/java/com/green/nowon/utils/PageDTO.java
+++ src/main/java/com/green/nowon/utils/PageDTO.java
@@ -15,29 +15,7 @@ public class PageDTO {
 	 * @param pageTotal 총 페이지수
 	 */
 	public PageDTO(int page, int pageTotal) {
-		//page=1: pGroup=1 pFrom=1;  pTo=5;
-		//page=2: pGroup=1 pFrom=1;  pTo=5;
-		//page=3: pGroup=1 pFrom=1;  pTo=5;
-		//page=4: pGroup=1 pFrom=1;  pTo=5;
-		//page=5: pGroup=1 pFrom=1;  pTo=5;
-		//page=6: pGroup=2 pFrom=6;  pTo=10;
-		//page=7: pGroup=2 pFrom=6;  pTo=10;
-		//page=8: pGroup=2 pFrom=6;  pTo=10;
-		//page=9: pGroup=2 pFrom=6;  pTo=10;
-		//page=10: pGroup=2 pFrom=6;  pTo=10;
-		int pLength=5;//페이지 그룹에 표현한 개수
-		
-		this.pageTotal=pageTotal;
-		int pGroup=page/pLength; 
-		// 1/5,2/5,3/5,4/5,5/5 == 1
-		//  0 , 0 , 0 , 0 , 1  == pGroup가 1이되도록
-		if(page%pLength !=0) pGroup++;
-		end=pGroup*pLength;
-		start=end-pLength+1;
-		
-		//혹시 마지막페이지번호는? 총 페이지수 보다 클수는 없어요
-		if(end > pageTotal) 
-			end=pageTotal;
+		this(page, pageTotal, 5);
 	}
 	
 	/**
